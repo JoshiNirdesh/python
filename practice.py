12466f9a423418cd12d4e3b25503374b731b1876
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-02-25 12:35:23 +0545

    if

---
+++ practice.py
@@ -0,0 +1,30 @@
+# print("Hello, Nirdesh Welcome to python")
+
+# name = "Nirdesh"
+# age = 23 
+# height = 5.8
+
+# print("Hello I am "+ name + "Age",age, "height",height)
+
+# name = input("Enter your name")
+# age = int(input("Enter a age"))
+
+# print("Hello, "+name+" you will be",age+1,"next year")
+
+# number = int(input("Enter a number "))
+
+# if number%2==0:
+#     print(number,"is even number")
+# else:
+#     print(number,"is odd number")
+
+marks = int(input("Enter a marks"))
+
+if marks >90 :
+    print("A+")
+elif marks>80 and marks<=90:
+    print("B")
+elif marks>70 and marks <=79:
+    print("C")
+else :
+    print("Fail")
