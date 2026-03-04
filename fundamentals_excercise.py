b7b6a5e7d909425b97af0b5cf8c31c80fac9f9dd
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-04 08:15:29 +0545

    prime number

--- fundamentals_excercise.py
+++ fundamentals_excercise.py
@@ -50,7 +50,7 @@
 #     if(num%2==0):
 #         print(num)  
 
-num = int(input("Enter a number "))
+# num = int(input("Enter a number "))
 
 # for nums in range(1,11):
 #     print(num," x ",nums," = ",num*nums)
@@ -58,6 +58,50 @@ num = int(input("Enter a number "))
 # for nums in range(1,num+1):
 #     fac *=nums
 # print("Factorials ", fac)
-rev = 0 
 
-while num >
+# text = input("Enter a string ")
+# print(text.upper())
+# print(text[::-1])
+
+# vowels = "aeiouAEIOU"
+# count=0
+# for char in text:
+#     if char in vowels:
+#         count+=1
+
+# print("Total vowels ", count)
+# rev = text[::-1]
+# if(text ==rev):
+#     print("Given string is palindrome")
+# else:
+#     print("given string is not plaindrom")
+
+# sen = input("Enter a sentence ")
+# count = 0
+# for word in sen:
+#     if word == " ":
+#         count +=1
+
+# print("Space ", count)
+
+# list = [1,45,23,12,2]
+# # print(max(list))
+# # print(min(list))
+# # print(sum(list))
+
+# for num in list:
+#     if num%2==0:
+#         print(num)
+# print(list.sort(reverse=True))
+# print(list)
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
