9e712b9f5e20f16ffafac6c7dea1183964939174
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-04 14:21:46 +0545

    personal navigator

--- textutils/textutils/textutils/view.py
+++ textutils/textutils/textutils/view.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello")
+    return HttpResponse('''<a href="https://www.youtube.com/watch?v=ES-bdt0KUZg&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=8" >Code with harry </a>''')
 
 def about (request):
     return HttpResponse("About Us")
