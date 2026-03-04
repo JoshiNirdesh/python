9ddb2afc8a37793f9ce3dd089c81d8f32cd75c55
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-04 08:19:39 +0545

    palindrome

--- fundamentals_excercise.py
+++ fundamentals_excercise.py
@@ -94,14 +94,23 @@
 #         print(num)
 # print(list.sort(reverse=True))
 # print(list)
-num = int(input("Enter a number: "))
+# num = int(input("Enter a number: "))
 
-if num <= 1:
-    print("Not Prime")
+# if num <= 1:
+#     print("Not Prime")
+# else:
+#     for i in range(2, num):
+#         if num % i == 0:
+#             print("Not Prime")
+#             break
+#     else:
+#         print("Prime")
+
+string = input("Enter a string ")
+
+rev = string[::-1]
+
+if(string == rev):
+    print("Palindrome")
 else:
-    for i in range(2, num):
-        if num % i == 0:
-            print("Not Prime")
-            break
-    else:
-        print("Prime")
+    print("Not palindrome")
