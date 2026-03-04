f1e2ae88473d53ac269bfd3256ddedd17059305e
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-04 16:29:46 +0545

    templates

--- textutils/textutils/textutils/view.py
+++ textutils/textutils/textutils/view.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Home") 
+    return render(request,"index.html")
 
 def rempunc(request):
     return HttpResponse("Remove Punchuation")
