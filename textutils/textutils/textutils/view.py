d4b1f592618ba580f1243c443909ec324703411d
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-06 08:14:35 +0545

    POST CSRF tokens

--- textutils/textutils/textutils/view.py
+++ textutils/textutils/textutils/view.py
@@ -6,11 +6,11 @@ def index(request):
     return render(request,"index.html")
 
 def analyze(request):
-    djtext = request.GET.get("text","default")
-    rempun =request.GET.get("rempunc","off")
-    fullcap = request.GET.get("fullcap","off")
-    newlinerem = request.GET.get("newlinerem","off")
-    countchar = request.GET.get("charcount","off")
+    djtext = request.POST.get("text","default")
+    rempun =request.POST.get("rempunc","off")
+    fullcap = request.POST.get("fullcap","off")
+    newlinerem = request.POST.get("newlinerem","off")
+    countchar = request.POST.get("charcount","off")
 
     if rempun=="on":
         punc = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
@@ -36,5 +36,7 @@ def analyze(request):
         for char in djtext:
             count+=1
         params = {"purpose":"Count Character","text":count}
+    else:
+        return HttpResponse("Error")
 
     return render(request,"analyze.html",params)
