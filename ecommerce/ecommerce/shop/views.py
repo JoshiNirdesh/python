b80ddd43f59afda101fb29438fed2dda2128580b
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-03-07 11:43:15 +0545

    shop urls

--- ecommerce/ecommerce/shop/views.py
+++ ecommerce/ecommerce/shop/views.py
@@ -3,3 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Shop Page")
+
+def about(request):
+    return HttpResponse("About us page")
+
+def contact(request):
+    return HttpResponse("Contact")
+
+def tracker(request):
+    return HttpResponse("tracker")
+
+def search(request):
+    return HttpResponse("search")
+
+def productview(request):
+    return HttpResponse("product view")
+
+def checkout(request):
+    return HttpResponse("checkout")
